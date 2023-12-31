2250e4e1a67e4ce3ae9f6d8c5136aa7792069fc7
Arfaqur Rahman <arfaqur@gmail.com>
2023-12-31 23:47:20 +0600

    Create 21. Calculate the simple interest for given principal, rate of interest, and time.java

---
+++ 21. Calculate the simple interest for given principal, rate of interest, and time.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    
+	   
+	    int P= 200;
+	    int time = 2;
+	    int rate = 2;
+	    int I = P*time*rate/100;
+	    System.out.println(I);
+	    
+	}
+	
+	
+}
